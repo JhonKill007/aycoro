b95840ec90077f6fb6114fe8fd211717350aa098
JhonKill007 <jhond2069@gmail.com>
2021-11-23 19:22:08 -0400

    ©Aycoro 2021 Version 0.6.1 almost_lanch connection

--- keys/conection.php
+++ keys/conection.php
@@ -2,17 +2,17 @@
 
 // localhost
 
-$host = "localhost";
-$dbusername = "root";
-$dbpassword =  "";
-$dbname = "aycoro";
+// $host = "localhost";
+// $dbusername = "root";
+// $dbpassword =  "";
+// $dbname = "aycoro";
 
 // hostserver
 
-// $host = "localhost";
-// $dbusername = "u811483169_aycoro";
-// $dbpassword =  "Siempreteamare007";
-// $dbname = "u811483169_aycoro";
+$host = "localhost";
+$dbusername = "u811483169_aycoro";
+$dbpassword =  "Siempreteamare007";
+$dbname = "u811483169_aycoro";
 
 
 $conn = new mysqli($host,$dbusername,$dbpassword,$dbname);
